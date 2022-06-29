8908ea46935fec56c083a5a96bd773df031b556b
Keyan <90410389+keyanUB@users.noreply.github.com>
2022-06-29 12:11:11 -0400

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ start_date = date(2021, 3, 1)
 until_date = date(2022, 5, 31)
 
 # ADD your Twitter API credentials here
-consumer_key = '8EXV0zj32NxakDHhNpkiRuM61'
-consumer_secret = 'pMrcvyEXc0VATNwtNtjRhJNPbDZyvmS1AR9ZuPF9HNQOkeMolV'
+consumer_key = ''
+consumer_secret = ''
 
-access_token = '1432557791735058434-lymMzALWwgbbKz5PLvfZQlfY4qdHhM'
-access_token_secret = 'KFb4uw1czenol12Udi5632YTDXGZAVTRdp7nE0JbwUmTw'
+access_token = ''
+access_token_secret = ''
