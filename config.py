d85c996c85d4cda4a225ea197fca93aff34e71b3
Keyan <90410389+keyanUB@users.noreply.github.com>
2022-07-17 11:30:14 -0400

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from datetime import date
 hashtags = ['#antivaxxers', '#VaccinesWork', '#CovidVaxFacts', '#vaccinesSaveLives']
 # MODIFY start and until date here
 start_date = date(2021, 3, 1)
-until_date = date(2022, 5, 31)
+until_date = date(2022, 1, 1)
 
 # ADD your Twitter API credentials here
-consumer_key = ''
-consumer_secret = ''
+consumer_key = 'Kwq1UTmJ0gv5FyDBng182hYtgg1kdALN5HAKUr5SDYlUhKFwTl'
+consumer_secret = 'AAAAAAAAAAAAAAAAAAAAAFUbYwEAAAAA2KG7UwKC%2Fb6vadfhvQJ1nMnmOY8%3DWaHZcZ1Q1cLDi2thgvQiom4SFoD0FU7nZaxoIpW0gkNDvM5Ozd'
 
-access_token = ''
-access_token_secret = ''
+access_token = '1432557791735058434-n0NcO5MQUPIpDcH8wpkkKVHqulyV7e'
+access_token_secret = 'KUyTj9OHvKzYmPltXWsLOek50uJpn7ViKIVK0QxFQlGED'
